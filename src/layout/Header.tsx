139b9be2ae86a8dd667f4e0c372f4b537c306325
Volodymyr Kysil <vavva67@gmail.com>
2023-12-21 21:57:21 +0100

    added cssBaseLine

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,10 +1,12 @@
 import ResponsiveAppBar from './AppBar'
+import CssBaseline from '@mui/material/CssBaseline'
 
 type Props = {}
 
 const Header = (props: Props) => {
     return (
         <>
+            <CssBaseline />
             <ResponsiveAppBar />
         </>
     )
