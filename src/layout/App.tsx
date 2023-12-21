5fe937b28ed89a7cfcf98774f7bbc7adea61b2cd
Volodymyr Kysil <vavva67@gmail.com>
2023-12-21 20:19:08 +0100

    added assets+layout

---
+++ src/layout/App.tsx
@@ -0,0 +1,17 @@
+import Footer from './Footer'
+import Header from './Header'
+import Main from './Main'
+
+type Props = {}
+
+const App = (props: Props) => {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    )
+}
+
+export default App
