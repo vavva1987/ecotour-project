cd541a2c9b8ba6959c53ecdd8c6602df952bab00
Volodymyr Kysil <vavva67@gmail.com>
2023-12-21 21:59:04 +0100

    added cssBaseLine upd

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,12 +1,15 @@
 import Footer from './Footer'
 import Header from './Header'
 import Main from './Main'
+import CssBaseline from '@mui/material/CssBaseline'
 
 type Props = {}
 
 const App = (props: Props) => {
     return (
         <>
+            <CssBaseline />
+
             <Header />
             <Main />
             <Footer />
