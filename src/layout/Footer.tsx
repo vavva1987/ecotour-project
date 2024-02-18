41046bd26798e59045ce5f71d9d6ddb8d6b7413d
Volodymyr Kysil <vavva67@gmail.com>
2024-02-18 21:00:47 +0100

    added Pages

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -10,18 +10,25 @@ const Footer = (props: Props) => {
     return (
         <Container
             maxWidth="lg"
-            sx={{
-                bgcolor: '#252424',
-                color: '#ffffff',
-                height: '470px',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}
+            sx={{ bgcolor: '#252424', color: '#ffffff', height: '470px' }}
         >
-            <FirstFooterComponent />
-            <SecondFooterComponent />
-            <ThirdFooterComponent />
+            <Container
+                sx={{
+                    height: '370px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <FirstFooterComponent />
+                <SecondFooterComponent />
+                <ThirdFooterComponent />
+            </Container>
+            <Container sx={{ borderTop: '1px solid #8ac92a', height: '100px' }}>
+                <div className="footer-bottom">
+                    © <span>ekotur</span> 2023. All rights reserved
+                </div>
+            </Container>
         </Container>
     )
 }
