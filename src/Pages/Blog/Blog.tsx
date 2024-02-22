f773826664e79ea738aef789c954ce776fa0cc4e
Volodymyr Kysil <vavva67@gmail.com>
2024-02-23 00:18:27 +0100

    added PagesBlogContent

--- src/Pages/Blog/Blog.tsx
+++ src/Pages/Blog/Blog.tsx
@@ -1,7 +1,24 @@
+import { Container } from '@mui/material'
+import './Blog.scss'
+import BlogTitle from './BlogTitle/BlogTitle'
+import BlogContent from './BlogContent/BlogContent'
+import BlogSidebar from './BlogSidebar/BlogSidebar'
+
 type Props = {}
 
 const Blog = (props: Props) => {
-    return <div>Blog</div>
+    return (
+        <Container className="blog">
+            <BlogTitle />
+            <Container
+                maxWidth="lg"
+                sx={{ display: 'flex', justifyContent: 'space-between' }}
+            >
+                <BlogContent />
+                <BlogSidebar />
+            </Container>
+        </Container>
+    )
 }
 
 export default Blog
--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardContent, Container, Grid } from '@mui/material'
+import { Button, Container, Grid } from '@mui/material'
 import './SectionTitle.scss'
 import SectionTitleContent from './SectionTitleContent'
 import { sectionArray } from 'utils/productsArray'
@@ -14,69 +14,75 @@ type Props = {
 
 const SectionTitle = (props: Props) => {
     return (
-        <Container maxWidth="lg" sx={{ marginTop: '65px' }}>
-            <Card>
-                <CardContent
+        <Container
+            maxWidth="lg"
+            sx={{
+                marginTop: '65px',
+                marginBottom: '30px',
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+        >
+            <Container
+                maxWidth="lg"
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    height: '100px',
+                    marginBottom: '15px',
+                }}
+            >
+                <div className="section-title">
+                    <div>
+                        <h2>
+                            Latest <span>posts</span>
+                        </h2>
+                    </div>
+                    <div>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore{' '}
+                        </p>
+                    </div>
+                </div>
+
+                <Button
+                    className="section-title-btn"
+                    variant="outlined"
                     sx={{
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                        height: '100px',
+                        width: '139px',
+                        height: '44px',
+                        color: 'black',
+                        border: '1px solid black',
+                        ':hover': {
+                            color: '#ffffff',
+                            backgroundColor: '#8ac92a',
+                            border: '1px solid black',
+                        },
+                        fontFamily: 'Raleway',
+                        fontSize: '14px',
+                        fontWeight: '700',
+                        fontStyle: 'normal',
                     }}
                 >
-                    <div className="section-title">
-                        <div>
-                            <h2>
-                                Latest <span>posts</span>
-                            </h2>
-                        </div>
-                        <div>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur
-                                adipiscing elit, sed do eiusmod tempor
-                                incididunt ut labore{' '}
-                            </p>
-                        </div>
-                    </div>
-
-                    <Button
-                        className="section-title-btn"
-                        variant="outlined"
-                        sx={{
-                            width: '139px',
-                            height: '44px',
-                            color: 'black',
-                            border: '1px solid black',
-                            ':hover': {
-                                color: '#ffffff',
-                                backgroundColor: '#8ac92a',
-                                border: '1px solid black',
-                            },
-                            fontFamily: 'Raleway',
-                            fontSize: '14px',
-                            fontWeight: '700',
-                            fontStyle: 'normal',
-                        }}
-                    >
-                        Show more
-                    </Button>
-                </CardContent>
-                <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
-                    <Container></Container>
-                    {sectionArray.map(
-                        ({ id, title, type, date, description, images }) => (
-                            <SectionTitleContent
-                                id={id}
-                                title={title}
-                                type={type}
-                                date={date}
-                                description={description}
-                                images={images}
-                            />
-                        )
-                    )}
-                </Grid>
-            </Card>
+                    Show more
+                </Button>
+            </Container>
+            <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
+                {sectionArray.map(
+                    ({ id, title, type, date, description, images }) => (
+                        <SectionTitleContent
+                            id={id}
+                            title={title}
+                            type={type}
+                            date={date}
+                            description={description}
+                            images={images}
+                        />
+                    )
+                )}
+            </Grid>
         </Container>
     )
 }
