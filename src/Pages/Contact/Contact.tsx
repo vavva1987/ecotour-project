6c2de27d67ed1fa7bb4195501db56e5a0b9b1283
Volodymyr Kysil <vavva67@gmail.com>
2024-02-27 22:56:16 +0100

    added ContactContent

--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -1,6 +1,8 @@
 import { Container } from '@mui/material'
 import './Contact.scss'
 import ContactHeader from './ContactComponents/ContactHeader'
+import ContactContent from './ContactComponents/ContactContent'
+import ContactForm from './ContactComponents/ContactForm'
 
 type Props = {}
 
@@ -8,6 +10,18 @@ const Contact = (props: Props) => {
     return (
         <Container sx={{ marginTop: '70px' }}>
             <ContactHeader />
+            <Container
+                sx={{
+                    bgcolor: '#d4d0d0',
+                    height: 632,
+                    display: 'flex',
+                    justifyContent: 'space-around',
+                    alignItems: 'center',
+                }}
+            >
+                <ContactContent />
+                <ContactForm />
+            </Container>
         </Container>
     )
 }
--- src/Pages/Contact/ContactComponents/ContactForm.tsx
+++ src/Pages/Contact/ContactComponents/ContactForm.tsx
@@ -1,7 +1,17 @@
+import { Container } from '@mui/material'
+import '../Contact.scss'
+
 type Props = {}
 
 const ContactForm = (props: Props) => {
-    return <div>ContactForm</div>
+    return (
+        <Container
+            maxWidth="sm"
+            sx={{ display: 'flex', flexDirection: 'column', width: 300 }}
+        >
+            ContactForm
+        </Container>
+    )
 }
 
 export default ContactForm
